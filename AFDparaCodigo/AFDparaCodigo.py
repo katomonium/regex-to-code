09944a893df73f123d5433e81c5390e9be6f19f6
joaopedroteo <jpteodoro2020@gmail.com>
2018-04-11 13:58:16 -0300

    gerando codigo sussu

--- AFDparaCodigo/AFDparaCodigo.py
+++ AFDparaCodigo/AFDparaCodigo.py
@@ -6,34 +6,62 @@ import sys
 from string import Template
 from Estado import Estado
 from Transicao import Transicao
+from Automato import Automato
 
-class GeradorDeCodigo():
-    t = Template('\tif(codigo[indice] == $letraTransicao):\n\t\tindice+=1\n\t\t$estadoDestino(codigo,indice)\n')
-    inicio = Template('def $nomeEstado(codigo, indice):\n')
-
-    def criaFuncao(self,estado):
-        resultado = self.inicio.substitute(nomeEstado = estado.idEstado)
-        for transicao in estado.transicoes:
-            resultado += self.t.substitute(letraTransicao = transicao.letra, estadoDestino = transicao.destino.idEstado)
+class GeradorDeCodigo:
+    
+    automato = None
+    
+    definicaoClasse = Template('def q$nomeEstado(codigo, indice):\n')
+    condicaoCaractere = Template('    if(codigo[indice] == "$letraTransicao"):\n        indice+=1\n        return q$estadoDestino(codigo,indice)\n')
+    condicaoFinal = Template('    if(indice == len(codigo)):\n        return $booleano\n')
+    cabecalho = "#!/usr/bin/env python3\n# -*- coding: utf-8 -*-\n\nimport sys\n\n"
+    mainPrograma = Template("def main(args):\n    arquivo = open(args[1], 'r')\n    indice = 0\n    linhas = arquivo.read().splitlines()\n    print($estadoInicial(linhas[0], indice))")
+    
+    def __init__(self,automato):
+        self.automato = automato
+    
+    def gerarCodigo(self):
+        resultado = self.cabecalho
+        for estado in self.automato.estados:
+            resultado += self.criaFuncao(estado)
+        print(self.automato.inicial + " okoko")
+        print(self.automato.estadosDic)
+        resultado += self.criaMain(self.automato.estadosDic[self.automato.inicial])
         return resultado
+        
+    
+    def criaFuncao(self,estado):
+        resultado = self.definicaoClasse.substitute(nomeEstado = estado.idEstado)
+        if estado.final:
+            resultado += self.condicaoFinal.substitute(booleano = 'True')
+        else:
+            resultado += self.condicaoFinal.substitute(booleano = 'False')
+        for transicao in estado.transicoes:
+            resultado += self.condicaoCaractere.substitute(letraTransicao = transicao.letra, estadoDestino = transicao.destino.idEstado)
+        resultado += "    return False\n\n"
+        return resultado
+        
+    def criaMain(self, estadoInicial):
+        return self.mainPrograma.substitute(estadoInicial = estadoInicial.idEstado)
 
 def AFDparaCodigo():
-    #~ entrada = sys.argv[1]
-    #~ saida = sys.argv[2]
+    if(len(sys.argv) < 3):
+        return
+    entrada = sys.argv[1]
+    saida = sys.argv[2]
     #~ automato = lerAutomato(entrada)
     #~ print(automato.inicial)
     
-    #~ criarArquivo()
-    e1 = Estado()
-    e2 = Estado()
-    e3 = Estado()
-    e3.idEstado = "e3"
-    e2.idEstado = "e2"
-    e1.idEstado = "e1"
-    t1 = Transicao(e3,"a",e1)
-    t2 = Transicao(e3,"b",e2)
-    e3.transicoes.append(t1)
-    e3.transicoes.append(t2)
+    a = Automato(entrada)
     
-    gdc = GeradorDeCodigo()
-    print(gdc.criaFuncao(e3))
+    
+    
+    
+    arquivo = open(sys.argv[2], 'w')
+    
+    gdc = GeradorDeCodigo(a)
+    arquivo.write(gdc.gerarCodigo())
+    print(gdc.gerarCodigo())
+    
+AFDparaCodigo()
---
+++ AFDparaCodigo/Automato.py
@@ -0,0 +1,47 @@
+from LeituraEscrita import Arquivo
+
+class Automato:
+    estados = None                  #lista de estados do automato
+    estadosDic = None               #dicionario que referencia os estados
+    alfabeto = None
+    inicial = None
+    finais = None                   #dicionario de estados finais
+    
+    pilha = None
+
+    #cria um dicionario de estados para o automato
+
+    def __init__(self, nomeArq = None):
+        self.pilha = []
+        
+        if(nomeArq == None):        #se nao for passado o nome do arquivo por parametro, cria-se um automato vazio
+            self.estados = []
+            self.estadosDic = {}
+            self.alfabeto = []
+            self.finais = {}
+            
+          
+
+        arquivo = Arquivo(nomeArq, 'r')         #instancia um objeto da classe arquivo (no arquivo LeituraEscrita)
+
+        #operacoes de leitura do arquivo
+        self.estados = arquivo.leEstados()
+        self.estadosDic = self.criaDicionario(self.estados)
+        print(self.estadosDic)
+        self.alfabeto = arquivo.leAlfabeto()
+        arquivo.leTransicoes(self)
+        # print(self.estadosDic)
+        self.inicial = arquivo.leInicial()
+        self.finais = arquivo.leFinais()
+        print(self.inicial)
+        #marca os estados finais
+        for i in range(len(self.estados)):
+            if(self.estados[i].idEstado in self.finais):
+                self.estados[i].final = True
+    
+    
+    def criaDicionario(self, estados):
+        estadosDic = {}
+        for i in range(len(estados)):
+            estadosDic[estados[i].idEstado] = estados[i]
+        return estadosDic
